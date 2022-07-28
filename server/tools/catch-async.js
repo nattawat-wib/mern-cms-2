e7bca801d91eebfe5837e81b4876464f00232ca7
nattawat-wib <nattawat.wib@gmail.com>
2022-07-28 21:43:47 +0700

    add comment

--- server/tools/catch-async.js
+++ server/tools/catch-async.js
@@ -2,7 +2,8 @@ const { UndoUploadFile } = require("./multer");
 
 module.exports = catchAsync = fn => (req, res, next) => {
     fn(req, res, next).catch(err => {
-        
+ 
+        // remove file that upload but fail
         if(req.files) UndoUploadFile(req.files)
 
         next(err)
